122f7eac6b43b27289cb822cb6a8df47945b20a0
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-12-18 15:10:16 +0100

    order public interface proposal

--- orders-api/src/main/java/pl/edu/wszib/orders/api/order/OrderFacadeApi.java
+++ orders-api/src/main/java/pl/edu/wszib/orders/api/order/OrderFacadeApi.java
@@ -1,5 +1,11 @@
 package pl.edu.wszib.orders.api.order;
 
 public interface OrderFacadeApi {
-    // TODO propose methods + arguments + results
+    OrderResult create(OrderCreateApi orderCreate);
+    OrderResult get(String orderId);
+    OrderResult addProduct(String orderId, String productId);
+    OrderResult removeProduct(String orderId, String productId);
+    OrderResult increaseProductQuantity(String orderId, String productId);
+    OrderResult decreaseProductQuantity(String orderId, String productId);
+    OrderResult setProductQuantity(String orderId, String productId, Integer quantity);
 }
