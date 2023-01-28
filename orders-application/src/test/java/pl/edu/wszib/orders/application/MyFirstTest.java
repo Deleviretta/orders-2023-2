fbec49a9915228d2611f225663becd6825a940ef
Natalia <natalia@wszib.edu.pl>
2023-01-29 00:12:24 +0100

    test sum method

--- orders-application/src/test/java/pl/edu/wszib/orders/application/MyFirstTest.java
+++ orders-application/src/test/java/pl/edu/wszib/orders/application/MyFirstTest.java
@@ -2,6 +2,9 @@ package pl.edu.wszib.orders.application;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyFirstTest {
@@ -10,4 +13,16 @@ public class MyFirstTest {
     public void should_be_able_to_find_existing_order() {
         assertTrue(true);
     }
+
+    @Test
+    public void should_sum_values() {
+        int result = sum(1, 2, 3, 4, 5);
+        assertEquals(12, result);
+    }
+
+    public static Integer sum(int... integers) {
+        return IntStream.of(integers).sum();
+    }
+
+
 }
