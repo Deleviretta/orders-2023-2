5f882f52f9925ef4fd74a48fb9212a73eed5ed99
Natalia <natalia@wszib.edu.pl>
2023-01-28 23:38:38 +0100

    first test

---
+++ orders-application/src/test/java/pl/edu/wszib/orders/application/MyFirstTest.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.orders.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyFirstTest {
+
+    @Test
+    public void should_be_able_to_find_existing_order() {
+        assertTrue(true);
+    }
+}
